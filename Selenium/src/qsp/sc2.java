76bad4dc8ac90baa412e6196b88fea5a4966db33
bshankarsg55@gmail.com <101615706+Bhimashankarsg@users.noreply.github.com>
2025-01-29 10:47:15 +0530

    dfdsf

---
+++ Selenium/src/qsp/sc2.java
@@ -0,0 +1,18 @@
+package introduction;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selintroduction {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver","D:/SOFTWARES/chromedriver-win64/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		//System.out.println(driver.getTitle());
+
+	}
+
+}
